d437b52125762f3f21284bafca7029992157d6c7
Lolboy505 <Lolboy505@mail.ru>
2026-02-08 23:47:50 +0300

    final 2ver optimiz size

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,6 @@
 import { Container, Row, Col, Nav } from "react-bootstrap"
 import { buttonStyle } from "../additional/buttonStyle"
 import redWall from '@/images/RedWall_light.webp';
-import style from './hero.module.css'
 import { phone } from "../additional/contactData";
 
 export default function Hero() {
