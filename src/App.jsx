e09f8e1c1e5947ea6c5a9c8905f6241b10509835
Lolboy505 <Lolboy505@mail.ru>
2026-02-22 23:29:30 +0300

    remove link preload in html in public, and refactoring public

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import redWallUrl from '@images/RedWall_light.webp';
+
+if (typeof window !== 'undefined') {
+  const link = document.createElement('link');
+  link.rel = 'preload';
+  link.as = 'image';
+  link.href = redWallUrl;
+  document.head.appendChild(link);
+}
+
 import Header from "@/components/header/Header"
 import MainData from "@/components/main/MainData"
 import Footer from '@/components/footer/Footer'
@@ -6,6 +16,7 @@ import Cookie from "@/components/cookie/Cookie.jsx"
 
 import '@/bootstrapAdd.css'
 import '@/components/additional/styles/redLine.css'
+
 
 export default function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
@@ -8,11 +8,14 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
-      // Теперь символ @ всегда указывает на папку src
       '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
     },
   },
   base:
